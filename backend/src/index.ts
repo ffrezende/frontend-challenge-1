6cb025c283780b4decc9e5c5baec45c52963d335
ffrezende <fabriciofraga15@gmail.com>
2024-10-30 15:27:32 -0400

    feat: [BE] simple post csv route

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,39 @@
-import { serve } from "@hono/node-server";
-import { Hono } from "hono";
+import { serve } from '@hono/node-server'
+import { Hono } from 'hono'
+import { cors } from 'hono/cors'
 
-const app = new Hono();
+import fs from 'fs'
 
-app.get("/", (c) => {
-  return c.text("Hello Hono!");
-});
+const uploadFolder = 'uploads'
 
-serve({ fetch: app.fetch, port: 8080 });
-console.log("Server is running on http://localhost:8080");
+const app = new Hono()
+app.use('/*', cors())
+
+app.post('/upload', async (c) => {
+  try {
+    const body = await c.req.parseBody()
+    const file = body['file'] as File
+
+    if (!file) {
+      return c.text('No file uploaded.', 400)
+    }
+
+    if (!fs.existsSync(uploadFolder)) {
+      fs.mkdirSync(uploadFolder, { recursive: true })
+    }
+
+    const arr = await file.arrayBuffer()
+
+    fs.writeFile(`${uploadFolder}/${file.name}`, Buffer.from(arr), (err) => {
+      if (err) throw err
+    })
+
+    return c.text('CSV file uploaded successfully!')
+  } catch (error) {
+    console.error(error)
+    return c.text('Error uploading file.', 500)
+  }
+})
+
+serve({ fetch: app.fetch, port: 8080 })
+console.log('Server is running on http://localhost:8080')
--- frontend/src/utils/hooks/useFileManagement.tsx
+++ frontend/src/utils/hooks/useFileManagement.tsx
@@ -17,7 +17,7 @@ export default function useFileManagement() {
 
   const formDataCSV = (csvString) => {
     const formData = new FormData()
-    formData.append('csv_file', new Blob([csvString], { type: 'text/csv' }), fileName)
+    formData.append('file', new Blob([csvString], { type: 'text/csv' }), fileName)
 
     return formData
   }
@@ -25,6 +25,24 @@ export default function useFileManagement() {
   const handleSubmitFile = async (headers, rows) => {
     const csvString = createCSVString(headers, rows)
     const csvFile = formDataCSV(csvString)
+
+    fetch('http://127.0.0.1:8080/upload', {
+      method: 'POST',
+      body: csvFile,
+    })
+      .then((response) => {
+        debugger
+        if (!response.ok) {
+          throw new Error('Network response was not ok')
+        }
+        return response.json()
+      })
+      .then((data) => {
+        console.log('CSV uploaded successfully:', data)
+      })
+      .catch((error) => {
+        console.error('Error uploading CSV:', error)
+      })
   }
 
   return { handleSubmitFile }
