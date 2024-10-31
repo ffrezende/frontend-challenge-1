3d9e11b50a6683778c83c437c55f96a3703190e5
ffrezende <fabriciofraga15@gmail.com>
2024-10-31 11:17:08 -0400

    feat: [BE] interfaces and enums

---
+++ backend/src/common/interface/index.ts
@@ -0,0 +1,53 @@
+import type { BillingClass, BillingCodeStandard } from '../enum/index.js'
+
+interface ReportingEntity {
+  reporting_entity_name: string
+  reporting_entity_type: string
+}
+
+interface Plan {
+  plan_name?: string
+  plan_id_type?: 'EIN' | 'HIOS'
+  plan_id?: string
+  plan_market_type?: 'group' | 'individual'
+}
+
+interface TaxIdentifier {
+  type: 'ein' | 'npi'
+  value: string
+}
+
+interface Provider {
+  billed_charge: number
+  npi: string[]
+}
+
+interface OutOfNetworkPayment {
+  allowed_amount: number
+  billing_code_modifier?: string[]
+  providers: Provider[]
+}
+
+interface AllowedAmount {
+  tin: TaxIdentifier
+  service_code?: string[]
+  billing_class: BillingClass
+  payments: OutOfNetworkPayment[]
+}
+
+interface OutOfNetwork {
+  name: string
+  billing_code_type: BillingCodeStandard
+  billing_code: string
+  billing_code_type_version: string
+  description: string
+  allowed_amounts: AllowedAmount[]
+}
+
+interface OONRates {
+  reporting_entity: ReportingEntity
+  plan?: Plan
+  out_of_network: OutOfNetwork[]
+  last_updated_on: string
+  version: string
+}
